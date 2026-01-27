ddb8eb1651c3f83fd370e71efe59cdf2ed097ffe
MohammedAqeel18 <aqeelworkmail@gmail.com>
2026-01-27 09:54:26 +0530

    feat: initial dashboard page and Dashboard layout page for shared ui across dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+export default function DashboardLayout({
+    children,
+}:{
+    children:React.ReactNode;
+}){ 
+    return(
+        <section>
+            <aside> Dashboard Sidebar</aside>
+            <main> {children}</main>
+        </section>
+    );
+}
